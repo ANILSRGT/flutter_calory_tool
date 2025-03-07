28ba789ca32bf5638635609abc53505e55235bf3
Anıl Sorgit <49879524+ANILSRGT@users.noreply.github.com>
2025-03-07 11:09:37 +0300

    Revamp CustomLineChart to enhance axis titles and improve gradient color representation

--- lib/presentation/widgets/charts/custom_line_chart.dart
+++ lib/presentation/widgets/charts/custom_line_chart.dart
@@ -28,82 +28,99 @@ class CustomLineChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      spacing: AppValues.sm.value,
-      children: [
-        Expanded(
-          child: LineChart(
-            LineChartData(
-              titlesData: const FlTitlesData(show: false),
-              borderData: FlBorderData(
-                border: Border.all(color: Colors.grey.shade300, width: 0.5),
-              ),
-              clipData: const FlClipData.all(),
-              gridData: FlGridData(
-                verticalInterval: 1,
-                horizontalInterval: (maxY - minY) / 5,
-                getDrawingHorizontalLine:
-                    (value) =>
-                        FlLine(color: Colors.grey.shade300, strokeWidth: 0.5),
-                getDrawingVerticalLine:
-                    (value) =>
-                        FlLine(color: Colors.grey.shade300, strokeWidth: 0.5),
-              ),
-              minX: 0,
-              maxX: data.length.toDouble() - 1,
-              minY: minY,
-              maxY: maxY,
-              lineBarsData: [
-                LineChartBarData(
-                  spots:
-                      data.asMap().entries.map((entry) {
-                        final index = entry.key;
-                        final spot = entry.value;
-                        return FlSpot(index.toDouble(), spot.y);
-                      }).toList(),
-                  isCurved: true,
-                  gradient: LinearGradient(
-                    colors:
-                        data.map((spot) {
+    return LineChart(
+      LineChartData(
+        titlesData: FlTitlesData(
+          rightTitles: const AxisTitles(),
+          topTitles: const AxisTitles(),
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: (maxY - minY) / 5,
+              getTitlesWidget: (value, meta) {
+                return Padding(
+                  padding: AppValues.sm.ext.padding.directional.right,
+                  child: Text(
+                    value.toStringAsFixed(0),
+                    textAlign: TextAlign.end,
+                    style: const TextStyle(color: Colors.black, fontSize: 12),
+                  ),
+                );
+              },
+            ),
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 1,
+              getTitlesWidget: (value, meta) {
+                return Padding(
+                  padding: AppValues.sm.ext.padding.directional.top,
+                  child: Text(
+                    data[value.toInt()].xLabel,
+                    style: const TextStyle(color: Colors.black, fontSize: 12),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        borderData: FlBorderData(
+          border: Border.all(color: Colors.grey.shade300, width: 0.5),
+        ),
+        clipData: const FlClipData.all(),
+        gridData: FlGridData(
+          verticalInterval: 1,
+          horizontalInterval: (maxY - minY) / 5,
+          getDrawingHorizontalLine:
+              (value) => FlLine(color: Colors.grey.shade300, strokeWidth: 0.5),
+          getDrawingVerticalLine:
+              (value) => FlLine(color: Colors.grey.shade300, strokeWidth: 0.5),
+        ),
+        minX: 0,
+        maxX: data.length.toDouble() - 1,
+        minY: minY,
+        maxY: maxY,
+        lineBarsData: [
+          LineChartBarData(
+            spots:
+                data.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final spot = entry.value;
+                  return FlSpot(index.toDouble(), spot.y);
+                }).toList(),
+            isCurved: true,
+            gradient: LinearGradient(
+              colors:
+                  data.map((spot) {
+                    return Color.lerp(
+                      Colors.greenAccent,
+                      Colors.redAccent,
+                      spot.y / maxY,
+                    )!;
+                  }).toList(),
+            ),
+            barWidth: 5,
+            isStrokeCapRound: true,
+            belowBarData: BarAreaData(
+              show: true,
+              gradient: LinearGradient(
+                colors:
+                    data
+                        .map((spot) {
                           return Color.lerp(
                             Colors.greenAccent,
                             Colors.redAccent,
                             spot.y / maxY,
                           )!;
-                        }).toList(),
-                  ),
-                  barWidth: 5,
-                  isStrokeCapRound: true,
-                  belowBarData: BarAreaData(
-                    show: true,
-                    gradient: LinearGradient(
-                      colors:
-                          data
-                              .map((spot) {
-                                return Color.lerp(
-                                  Colors.greenAccent,
-                                  Colors.redAccent,
-                                  spot.y / maxY,
-                                )!;
-                              })
-                              .map((color) => color.withValues(alpha: 0.6))
-                              .toList(),
-                    ),
-                  ),
-                ),
-              ],
+                        })
+                        .map((color) => color.withValues(alpha: 0.6))
+                        .toList(),
+              ),
             ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            for (final spot in data)
-              Text(spot.xLabel, style: context.ext.theme.textTheme.bodySmall),
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
